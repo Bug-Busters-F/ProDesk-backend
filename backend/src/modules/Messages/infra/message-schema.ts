d9d7a4115680e8abbc0f6f53d9d43895317b3c90
ViniElias <viniciuseliascr@gmail.com>
2026-03-16 11:46:49 -0300

    add message-schema

---
+++ backend/src/modules/Messages/infra/message-schema.ts
@@ -0,0 +1,24 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+
+export type MessageDocument = Message & Document;
+
+@Schema({ timestamps: true, collection: 'messages'})
+export class Message {
+    @Prop({ required: true, type: Types.ObjectId })
+    chatId: Types.ObjectId; // Referência ao chamado
+
+    @Prop({ required: true, type: Types.ObjectId })
+    senderId: Types.ObjectId; // Id do cliente ou atendente que enviou a mensagem
+
+    @Prop({ required: true })
+    content: string; // Conteúdo do chat
+
+    @Prop({ default: false })
+    isSystemMessage: boolean; // Flag para mensagens da URA digital
+
+    @Prop({ required: false })
+    readAt?: Date; // Controle pra saber se a mensagem foi lida
+}
+
+export const MessageSchema = SchemaFactory.createForClass(Message);
