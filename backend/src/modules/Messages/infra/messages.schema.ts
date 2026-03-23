5c584e3bd00b5b9ff36050f3ab3e19b6c3580d83
Ygor Pereira <107441120+YgorPereira@users.noreply.github.com>
2026-03-23 20:45:26 -0300

    feat: database in memory for integration tests (#5)
    * feat: database in memory for integration tests
    
    * feat: add integration tests step in ci pipeline

--- backend/src/modules/Messages/infra/messages.schema.ts
+++ backend/src/modules/Messages/infra/messages.schema.ts
@@ -3,22 +3,22 @@ import { Document, Types } from 'mongoose';
 
 export type MessageDocument = Message & Document;
 
-@Schema({ timestamps: true, collection: 'messages'})
+@Schema({ timestamps: true, collection: 'messages' })
 export class Message {
-    @Prop({ required: true, type: Types.ObjectId })
-    chatId: Types.ObjectId; // Referência ao chamado
+  @Prop({ required: true, type: Types.ObjectId })
+  chatId: Types.ObjectId; // Referência ao chamado
 
-    @Prop({ required: true, type: Types.ObjectId })
-    senderId: Types.ObjectId; // Id do cliente ou atendente que enviou a mensagem
+  @Prop({ required: true, type: Types.ObjectId })
+  senderId: Types.ObjectId; // Id do cliente ou atendente que enviou a mensagem
 
-    @Prop({ required: true })
-    content: string; // Conteúdo do chat
+  @Prop({ required: true })
+  content: string; // Conteúdo do chat
 
-    @Prop({ default: false })
-    isSystemMessage: boolean; // Flag para mensagens da URA digital
+  @Prop({ default: false })
+  isSystemMessage: boolean; // Flag para mensagens da URA digital
 
-    @Prop({ required: false })
-    readAt?: Date; // Controle pra saber se a mensagem foi lida
+  @Prop({ required: false })
+  readAt?: Date; // Controle pra saber se a mensagem foi lida
 }
 
 export const MessageSchema = SchemaFactory.createForClass(Message);
