a1fdd95ca008c4cb09c76db5ae234f8ef2c6d47d
ViniElias <viniciuseliascr@gmail.com>
2026-03-23 11:30:43 -0300

    Alteração no app.module.ts para testes. Funcionalidade do WebSocket testada

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './modules/database/database.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MessageModule } from './modules/Messages/message.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { AppService } from './app.service';
       isGlobal: true,
     }),
     DatabaseModule,
+    MessageModule
   ],
   controllers: [AppController],
   providers: [AppService],
