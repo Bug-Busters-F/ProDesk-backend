928a439bd80a26f2dc813e7e14fa039b8996818f
Ygor Pereira <107441120+YgorPereira@users.noreply.github.com>
2026-04-02 10:03:57 -0400

    Feat/ticket module (#8)
    * feat: tickect domain entity structure
    
    * feat: hydrating ticket domain entity
    
    * feat: finished entity model for ticket artefact with  tests
    
    * refactor: new enums to erros and event messages
    
    * feat: ticket schema v1
    
    * feat: initialize repository with an abstract class and test
    
    * feat: ticket repository v1
    
    * feat: finish repository with save function and improve the tests
    
    * refactor: remove priority fiel from create and constructor input
    
    * feat: usecases v1
    
    * fix: fixing jest int config json
    
    * feat: finished application layer v1
    
    * feat: finish controller v1
    
    * feat: module configuration and inject dependency
    
    * feat: add ticket routes on swagger
    
    ---------
    
    Co-authored-by: YgorPereira <ygor.pereira@autaza.com>

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,19 +5,20 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors()
-    app.setGlobalPrefix("ProDeskApi")
-    app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+  app.setGlobalPrefix('ProDeskApi');
+  app.useGlobalPipes(new ValidationPipe());
 
-    const config = new DocumentBuilder()
+  const config = new DocumentBuilder()
     .setTitle('ProDesk')
     .setDescription('The ProDesk API')
     .setVersion('1.0')
     .addBearerAuth()
     .build();
-    const documentFactory = () => SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, documentFactory);
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   await app.listen(process.env.PORT ?? 3000);
 }
---
+++ backend/src/modules/ticket/application/useCases/readAll/readAll.usecase.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@nestjs/common';
+import {
+  Ticket,
+  TicketCategory,
+  TicketPriority,
+  TicketStatus,
+} from '../../../domain/entities/ticket.entity';
+import { ITicketRepository } from '../../../domain/repository/ticket.repository.interface';
+
+export interface ReadAllTicketOutput {
+  id: string;
+  title: string;
+  category: TicketCategory;
+  priority: TicketPriority;
+  description: string;
+  clientId: string;
+  status: TicketStatus;
+  agentId: string | null;
+  groupId: string | null;
+  escalationLevel: number;
+  createdAt: Date;
+  updatedAt: Date | null;
+  closedAt: Date | null;
+}
+
+@Injectable()
+export class ReadAllTicketUseCase {
+  constructor(private readonly repository: ITicketRepository) {}
+
+  async execute(): Promise<ReadAllTicketOutput[]> {
+    const foundedTickets = await this.repository.readAll();
+
+    const convertedTickets = foundedTickets.map((t: Ticket) => {
+      const primitive = t.toPrimitives();
+
+      return {
+        id: primitive._id,
+        title: primitive.title,
+        category: primitive.category,
+        priority: primitive.priority,
+        description: primitive.description,
+        clientId: primitive.clientId,
+        status: primitive.status,
+        agentId: primitive.agentId,
+        groupId: primitive.groupId,
+        escalationLevel: primitive.escalationLevel,
+        createdAt: primitive.createdAt,
+        updatedAt: primitive.updatedAt,
+        closedAt: primitive.closedAt,
+      };
+    });
+
+    return convertedTickets;
+  }
+}
