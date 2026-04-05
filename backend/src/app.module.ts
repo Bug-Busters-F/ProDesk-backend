4539dc8395818e446cdd828f3aef8742c590f424
Gabriel Viell Castilho <gabrielviell@hotmail.com>
2026-04-05 20:58:37 -0300

    fix: merge conflicts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
-<<<<<<< HEAD
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './modules/database/database.module';
 import { UserModule } from './modules/user/user.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { GroupModule } from './modules/group/group.module';
 import { CompanyModule } from './modules/company/company.module';
-import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseModule } from '@nestjs/mongoose';  
 import { MessageModule } from './modules/Messages/message.module';
 import { ChatModule } from './modules/chat/chat.module';
 import { TriageModule } from './modules/triage/triage.module';
@@ -32,14 +31,5 @@ import { TicketModule } from './modules/ticket/ticket.module';
   ],
   controllers: [],
   providers: [],
-=======
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
-@Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
->>>>>>> main
 })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-<<<<<<< HEAD
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import morgan from 'morgan';
@@ -27,11 +26,6 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
-=======
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
->>>>>>> main
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
